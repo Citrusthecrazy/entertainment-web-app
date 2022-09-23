439d8840ad63842944bf3e14df9db6d41715aa54
Marko <drakulic.marko0@gmail.com>
2022-09-23 13:16:56 +0200

    Bookmarking works as it should

--- src/components/BookmarkButton/index.tsx
+++ src/components/BookmarkButton/index.tsx
@@ -1,18 +1,48 @@
-import React, { HTMLProps } from "react";
+import React, { HTMLProps, useState } from "react";
 import bookmarkEmpty from "../../assets/icon-bookmark-empty.svg";
 import bookmarkFilled from "../../assets/icon-bookmark-full.svg";
 
 type Props = {
   mediaId: number;
+  isBookmarked?: boolean;
 };
 
 const BookmarkButton = ({
   className,
   mediaId,
 }: HTMLProps<HTMLButtonElement> & Props) => {
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(() =>
+    JSON.parse(localStorage.getItem("bookmarkedItems") || "[]").includes(
+      mediaId
+    )
+  );
+
+  const handleBookmark = () => {
+    const bookmarkedItems = JSON.parse(
+      localStorage.getItem("bookmarkedItems") || "[]"
+    ) as number[];
+    if (bookmarkedItems.includes(mediaId)) {
+      const newBookmarks = bookmarkedItems.filter((id) => id !== mediaId);
+      localStorage.setItem("bookmarkedItems", JSON.stringify(newBookmarks));
+      setIsBookmarked(false);
+    } else {
+      const bookmarkedItems = JSON.parse(
+        localStorage.getItem("bookmarkedItems") || "[]"
+      ) as number[];
+      const newBookmarks = [...bookmarkedItems, mediaId];
+      localStorage.setItem("bookmarkedItems", JSON.stringify(newBookmarks));
+      setIsBookmarked(true);
+    }
+  };
+
   return (
-    <button className={`bg-darkBlue/50 rounded-full p-2 ${className}`}>
-      <img src={bookmarkEmpty} alt="bookmark" />
+    <button
+      className={`bg-darkBlue/50 rounded-full p-2 ${className}`}
+      onClick={() => handleBookmark()}>
+      <img
+        src={!isBookmarked ? bookmarkEmpty : bookmarkFilled}
+        alt="bookmark"
+      />
     </button>
   );
 };
---
+++ src/pages/BookmarksPage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookmarksPage = () => {
+  return <main>BookmarksPage</main>;
+};
+
+export default BookmarksPage;
